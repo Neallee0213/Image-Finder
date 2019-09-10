4b56260c20283d2a5e5c951fc9c7c01d9dbc170e
Neallee0213 <hyunjoon.lee0213@gmail.com>
2019-09-10 18:20:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+// import NavBar from "./components/navbar/NavBar";
+import Search from "./components/search/Search";
+import Box from '@material-ui/core/Box';
+import { Grid } from "@material-ui/core";
+import { fontWeight, letterSpacing } from '@material-ui/system';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component  {
+  render(){
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Grid container justify="center">
+            <Box 
+            
+            style ={{ 
+            backgroundImage: "linear-gradient(to left bottom, #0464f5, #387cf7, #5b94f7, #7daaf4, #9fbff0)",
+            width:"100%",
+            color: "white",
+            lineHeight: "120px",
+            textAlign:"center",
+            textTransform:"uppercase",
+            fontSize:"200%",
+            fontWeight:"200",
+            letterSpacing:"20px"
+          }} >
+              Image Finder Machine
+            </Box>
+          </Grid>
+          <Search/>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
